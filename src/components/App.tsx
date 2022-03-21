033c77fb4869598d0d66073acd7d31b82ec9ffe4
Kayque Avelar <slooock@Kayques-iMac.local>
2022-03-21 18:39:23 -0300

    style

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,16 +6,39 @@ import {
   useGlobalSettings,
 } from "../hooks/useGlobalSettings";
 import { StatisticsContext, useStatistics } from "../hooks/useStatistics";
+import Button from "./Button";
+import { useState } from "react";
 
 function App() {
   const globalSettings = useGlobalSettings();
   const statistics = useStatistics();
+  const [explore, setExplore] = useState<boolean>(true);
 
   return (
     <StatisticsContext.Provider value={statistics}>
       <GlobalSettingsContext.Provider value={globalSettings}>
         <div className="app-container">
-          <Game />
+          {explore ? (
+            <div>
+              <h1>A jornada</h1>
+              <div>
+                "Que haja uma luz nos lugares mais escuros, quando todas as
+                outras luzes se apagarem."
+              </div>
+              <div className="subTitle">
+                responda as perguntas e encontre seu destino.
+              </div>
+              <Button
+                onClick={() => {
+                  setExplore(false);
+                }}
+                className="mt-3"
+                label="EXPLORAR"
+              />
+            </div>
+          ) : (
+            <Game />
+          )}
         </div>
       </GlobalSettingsContext.Provider>
     </StatisticsContext.Provider>
--- src/components/StatisticsView.tsx
+++ src/components/StatisticsView.tsx
@@ -1,7 +1,10 @@
 import { useContext, useEffect, useState } from "react";
 import { GlobalSettingsContext } from "../hooks/useGlobalSettings";
-import { GuessDistributionKeys, StatisticsContext } from "../hooks/useStatistics";
-import '../styles/StatisticsView.css';
+import {
+  GuessDistributionKeys,
+  StatisticsContext,
+} from "../hooks/useStatistics";
+import "../styles/StatisticsView.css";
 
 const COUNTDOWN_SECOND = 1000;
 const DISTRIBUTION_FIXED_WIDTH = 12;
@@ -17,27 +20,28 @@ const getCountdownString = () => {
   const offsetInMs = tomorrowDate.getTime() - todayDate.getTime();
 
   const offsetInSeconds = Math.floor(offsetInMs / 1000);
-  const seconds = (offsetInSeconds % 60).toString().padStart(2, '0');
+  const seconds = (offsetInSeconds % 60).toString().padStart(2, "0");
 
   const offsetInMinutes = Math.floor(offsetInSeconds / 60);
-  const minutes = (offsetInMinutes % 60).toString().padStart(2, '0');
+  const minutes = (offsetInMinutes % 60).toString().padStart(2, "0");
 
   const offsetInHours = Math.floor(offsetInMinutes / 60);
-  const hours = offsetInHours.toString().padStart(2, '0');
+  const hours = offsetInHours.toString().padStart(2, "0");
 
   return `${hours}:${minutes}:${seconds}`;
-}
+};
 
 export function StatisticsView() {
-  const [{isColorblindModeActive}] = useContext(GlobalSettingsContext);
+  const [{ isColorblindModeActive }] = useContext(GlobalSettingsContext);
 
-  const [{
-    distribution, currentStreak, maxStreak,
-  }] = useContext(StatisticsContext);
+  const [{ distribution, currentStreak, maxStreak }] =
+    useContext(StatisticsContext);
 
   const [countdown, setCountdown] = useState<string>(getCountdownString());
 
-  const distributionKeys = Object.keys(distribution).filter(key => key !== 'X') as GuessDistributionKeys[];
+  const distributionKeys = Object.keys(distribution).filter(
+    (key) => key !== "X"
+  ) as GuessDistributionKeys[];
   const distributionValues = Object.values(distribution) as number[];
 
   const maxDistribution = Math.max(...distributionValues);
@@ -46,14 +50,13 @@ export function StatisticsView() {
   const wonGames = allGames - distribution.X;
 
   const wonPercentage = allGames !== 0 ? (wonGames / allGames) * 100 : 0;
-  const wonPercentageStr =
-    Number.isSafeInteger(wonPercentage)
-      ? `${wonPercentage}%`
-      : `${wonPercentage.toFixed(1).replace('.', ',')}%`
+  const wonPercentageStr = Number.isSafeInteger(wonPercentage)
+    ? `${wonPercentage}%`
+    : `${wonPercentage.toFixed(1).replace(".", ",")}%`;
 
   const getVariableWidth = (distributionValue: number) => {
     return (distributionValue / maxDistribution) * DISTRIBUTION_VARIABLE_WIDTH;
-  }
+  };
 
   useEffect(() => {
     const countdownInterval = setInterval(() => {
@@ -61,68 +64,12 @@ export function StatisticsView() {
     }, COUNTDOWN_SECOND);
 
     return () => clearInterval(countdownInterval);
-  })
+  });
 
   return (
-    <div>
-      <h3 className="text-center">estatísticas</h3>
-
-      <div className="row text-center">
-        <div className="col-3 d-flex flex-column">
-          <h4>{ allGames }</h4>
-          <span className='data-label'>letrecos jogados</span>
-        </div>
-
-        <div className="col-3 d-flex flex-column">
-          <h4>{ wonPercentageStr }</h4>
-          <span className='data-label'>de vitória no jogo</span>
-        </div>
-
-        <div className="col-3 d-flex flex-column">
-          <h4>{ currentStreak }</h4>
-          <span className='data-label'>sequência atual</span>
-        </div>
-
-        <div className="col-3 d-flex flex-column">
-          <h4>{ maxStreak }</h4>
-          <span className='data-label'>maior sequência</span>
-        </div>
-      </div>
-
-      <div className="row mt-4">
-        <div className="col-7">
-          <div className="w-100 h-100">
-            <p className="text-center mb-1">distribuição das partidas</p>
-
-            {
-              distributionKeys.map((key, index) => (
-                <div
-                  key={key}
-                  className={'d-flex ' + (index !== distributionKeys.length - 1 ? 'mb-1' : '')}
-                >
-                  <span className="me-1">{key}</span>
-                  <div
-                    className={
-                      'h-100 px-2 graph-bar d-flex justify-content-end'
-                      + (isColorblindModeActive ? ' colorblind' : '')
-                    }
-                    style={{
-                      width: `${DISTRIBUTION_FIXED_WIDTH + getVariableWidth(distribution[key])}%`,
-                    }}
-                  >
-                    <span>{distribution[key]}</span>
-                  </div>
-                </div>
-              ))
-            }
-          </div>
-        </div>
-
-        <div className="col-5 d-flex flex-column text-center justify-content-center">
-          <span>próximo letreco em:</span>
-          <h4>{ countdown }</h4>
-        </div>
-      </div>
+    <div className="col-5 d-flex flex-column text-center justify-content-center">
+      <span>A nova pista estará disponível em:</span>
+      <h4>{countdown}</h4>
     </div>
   );
-};
+}
